eb105f2456bce85064b169b33bad1e3f900df29d
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-12-16 20:40:17 +0000

    made input field component and  started UI

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,15 +1,50 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input
+} from '@chakra-ui/react'
+import Wrapper from '../components/wrapper'
+import { InputField } from '../components/InputField'
 
 interface registerProps {}
 
 const register: React.FC<registerProps> = ({}) => {
   return (
-    <Formik
-      initialValues={{ username: '', password: '' }}
-      onSubmit={(v) => console.log(v)}>
-      {() => <Form>Hello</Form>}
-    </Formik>
+    <Wrapper variant='small'>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={(v) => console.log(v)}>
+        {(props) => (
+          <Form>
+            <InputField
+              name='username'
+              placeholder='username'
+              label='Username'
+            />
+            <Box mt={4}>
+              <InputField
+                name='password'
+                placeholder='password'
+                label='Password'
+                type='password'
+              />
+            </Box>
+            <Button
+              mt={4}
+              type='submit'
+              colorScheme='teal'
+              isLoading={props.isSubmitting}>
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   )
 }
 
