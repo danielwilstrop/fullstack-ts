4fdefafcb5f71a567aaca6c40bf62089161e9cdf
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-12-18 20:09:06 +0000

    fetch from server working for register mutation

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -10,15 +10,32 @@ import {
 } from '@chakra-ui/react'
 import Wrapper from '../components/wrapper'
 import { InputField } from '../components/InputField'
+import { useMutation } from 'urql'
 
 interface registerProps {}
 
+const REGISTER_MUTATION = `mutation Register($username: String!, $password: String!) {
+  register(options:  {username: $username, password: $password}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`
+
 const register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUTATION)
+
   return (
     <Wrapper variant='small'>
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={(v) => console.log(v)}>
+        onSubmit={(values) => register(values)}>
         {(props) => (
           <Form>
             <InputField
